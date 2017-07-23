70f394fc35dd73a5342d88b304baba118c5a15bf
stalker_alex92 <qwe23011992qwe>
2017-07-23 16:51:29 +0800

    26 lesson, date/time intent filter

---
+++ p0261intentfilter/src/main/java/ru/startandroid/intentfilter/MainActivity.java
@@ -0,0 +1,39 @@
+package ru.startandroid.intentfilter;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        Button btnTime = (Button) findViewById(R.id.btnTime);
+        Button btnDate = (Button) findViewById(R.id.btnDate);
+
+        btnTime.setOnClickListener(this);
+        btnDate.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent;
+
+        switch(v.getId()) {
+            case R.id.btnTime:
+                intent = new Intent("ru.startandroid.intent.action.showtime");
+                startActivity(intent);
+                break;
+            case R.id.btnDate:
+                intent = new Intent("ru.startandroid.intent.action.showdate");
+                startActivity(intent);
+                break;
+        }
+    }
+}
